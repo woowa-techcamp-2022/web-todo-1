f3bb922712d52b2af2fee7cb42e2f5f083231601
youngkyo0504 <keumky2@gmail.com>
2022-07-11 21:57:29 +0900

    💄 drawer 스타일 , drawer클릭 이벤트 애니메이션 가능하게 클릭 이벤트 넣었음 #3

--- src/app.js
+++ src/app.js
@@ -1,2 +1,16 @@
 import "./reset.css";
 import "./style.css";
+
+const menuBtn = document.querySelector(".menu");
+const drawer = document.querySelector(".drawer");
+const closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", () => {
+  console.log(drawer);
+  drawer.classList.toggle("open");
+  drawer.style.transition = "ease-in 0.3s transform";
+});
+
+closeBtn.addEventListener("click", () => {
+  drawer.classList.remove("open");
+});
