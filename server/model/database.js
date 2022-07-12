28bd2c5446c24ff8fbd7192e65e9bc1ab39e9e27
padosum <makalu171@gmail.com>
2022-07-12 10:38:04 +0900

    🔧 dotenv 사용 DB 정보 환경변수 설정하기 #4

--- server/model/database.js
+++ server/model/database.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 // get the client
 const mysql = require("mysql2");
 
 // create the connection to database
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
-  database: "test",
-  password: "1234",
+  user: process.env.DATABSE_USERNAME,
+  database: process.env.DATABSE_NAME,
+  password: process.env.DATABSE_PASSWORD,
 });
 
 module.exports = db;
