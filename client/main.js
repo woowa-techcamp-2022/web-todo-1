157c72b282efdebb432b773bd6088f83009a780f
youngkyo0504 <keumky2@gmail.com>
2022-07-13 18:58:48 +0900

    🔥 불필요한 파일 삭제
    - MVC 관련

--- client/main.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  const todoListContainer = qs(".todo-list-wrapper");
-  const actionStackContainer = qs(".action-stack");
-
-  // todoList 인스턴스를 만들어서 초기화
-  // actionStack 인스턴스를 만들고 초기화
-}
-
-const app = new App();
--- client/model/Store.js
+++
@@ -1,7 +0,0 @@
-export default class Store {
-  constructor(storage) {
-    if (!storage) throw new Error("no storage");
-
-    this.storage = storage;
-  }
-}
