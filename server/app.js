efd96c758c4ce78a9be5de16c6f6c1d28c92cbda
youngkyo0504 <keumky2@gmail.com>
2022-07-14 20:30:18 +0900

    ✨ server todo get api /todo get 받으면 todolist 반환

--- server/app.js
+++ server/app.js
@@ -5,6 +5,24 @@ const app = express();
 
 app.listen(process.env.PORT || "3000", () => {});
 
-app.get("/", (req, res) => {
-  pool.query("SELECT * FROM TODO_LIST").then((data) => res.json(data[0]));
+app.get("/todo", (req, res) => {
+  const todoList = {};
+  pool.query("SELECT * FROM TODO_LIST").then((data) => {
+    const [columns] = [...data];
+
+    const promise = columns.map((column) => {
+      todoList[column.ID] = { name: column.TITLE };
+      return pool
+        .query(`SELECT * FROM TASKS WHERE LIST_ID=${column.ID}`)
+        .then((result) => {
+          const tasks = result[0];
+          todoList[column.ID] = {
+            name: column.TITLE,
+            tasks,
+          };
+        });
+    });
+
+    Promise.all(promise).then(() => res.json(todoList));
+  });
 });
