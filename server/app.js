060268e6089497c5f776469f529e5c11977c03ca
youngkyo0504 <keumky2@gmail.com>
2022-07-14 21:37:04 +0900

    💥 프론트앤드 빌드 리스소 서버 연결 프론트단에서 빌드한 웹앱을 node.js의 static 파일로 연결했다.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const createError = require("http-errors");
 // const bodyParser = require("body-parser");
 const pool = require("./model/database");
 
 const app = express();
 app.use(express.json());
+app.use(express.static("dist"));
 
 app.listen(process.env.PORT || "3000", () => {});
 
